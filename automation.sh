ec7bab014b18fe0616d9b61ab9a0d764f7e28db0
sambitpulak <sambit.pulak@gmail.com>
2022-07-18 07:58:47 +0000

    Added inventory and cron jobs

--- automation.sh
+++ automation.sh
@@ -22,3 +22,24 @@ tar cvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
 aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
 s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+filename="/tmp/${myname}-httpd-logs-${timestamp}.tar"
+file_size=$(du -sh $filename | awk '{print $1}')
+
+cat /var/www/html/inventory.html
+if [ $? -eq 1 ]; then
+        touch inventory.html
+        echo "Log Type          Time Created            Type            Size" > /var/www/html/inventory.html
+        echo "httpd-logs                ${timestamp}            tar             ${file_size}" >> /var/www/html/inventory.html
+else
+        echo "httpd-logs                ${timestamp}            tar             ${file_size}" >> /var/www/html/inventory.html
+fi
+
+
+cat /etc/cron.d/automation
+if [ $? -eq 1 ]; then
+        echo "File does not exist"
+        touch automation
+        echo "PATH=/root/Automation_Project/automation.sh" > /etc/cron.d/automation
+        echo "0 1 * * *         root            /root/Automation_Project/automation.sh" >> /etc/cron.d/automation
+fi
